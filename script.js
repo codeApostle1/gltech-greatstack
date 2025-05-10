da4fe84f0e08742f860aa1cbfe27232edd3d59fd
J-max <joelmtn7@gmail.com>
2025-05-10 23:31:38 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+console.log('hii');
+
+
+// object destructuring
+// const person = {
+//     firstName: 'John',
+//     lastName: 'Doe',
+// }
+
+// let {firstName, lastName} = person;
+// console.log(firstName);
+
+
+// ***************
+
+// js class
+
+class ClassName {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+
+const person1 = new ClassName('John', 30);
+
+console.log(person1.name);
+
+
